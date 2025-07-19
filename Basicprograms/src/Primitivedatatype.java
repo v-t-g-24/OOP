0d8dd9f6daecb38d796be16fc607cfe3481c6d35
v-t-g-24 <tanish3139@gmail.com>
2025-07-19 11:32:18 +0530

    Add files via upload

---
+++ Basicprograms/src/Primitivedatatype.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Primitivedatatype {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        // Check if the number is even or odd
+        if (number % 2 == 0) {
+            System.out.println("The number is even.");
+        } else {
+            System.out.println("The number is odd.");
+        }
+
+        scanner.close();
+    }
+}
